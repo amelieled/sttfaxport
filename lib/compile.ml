4f5e14019e48debd794034d48e9db7c79988f67d
hondet <hondet@nancy.private.lsv.fr>
2022-07-08 10:50:24 +0200

    Using logging library

--- lib/compile.ml
+++ lib/compile.ml
@@ -1,4 +1,3 @@
-open Console
 open Ast
 open Sttfadk
 open Environ
@@ -7,28 +6,21 @@ module CTerm = Compile_term
 module CProof = Compile_proof
 module Term = Kernel.Term
 module Entry = Parsers.Entry
-
-let log_sttfa = new_logger "stfc"
-let log_sttfa = log_sttfa.logger
+module Dpp = Api.Pp.Default
 
 let compile_declaration dkenv name ty =
   match ty with
   | Term.App (cst, a, []) when is_sttfa_const sttfa_etap cst ->
-      (* Format.eprintf "[COMPILE] parameter: %a@." Pp.print_name name ; *)
       let ty' = CType.compile_type dkenv empty_env a in
       Parameter (of_name name, ty')
   | Term.App (cst, a, []) when is_sttfa_const sttfa_eta cst ->
-      (* Format.eprintf "[COMPILE] parameter: %a@." Pp.print_name name ; *)
       let ty' = CType.compile__type dkenv empty_env a in
       Parameter (of_name name, Ty ty')
   | Term.App (cst, a, []) when is_sttfa_const sttfa_eps cst ->
-      (* Format.eprintf "[COMPILE] axiom: %a@." Pp.print_name name ; *)
       let te' = CTerm.compile_term dkenv empty_env a in
       Axiom (of_name name, te')
   | _ ->
-      if Sttfadk.is_tyop ty then
-        (* Format.eprintf "[COMPILE] typeop: %a@." Pp.print_name name ; *)
-        TypeDecl (of_name name, arity_of_tyop ty)
+      if Sttfadk.is_tyop ty then TypeDecl (of_name name, arity_of_tyop ty)
       else assert false
 
 let compile_definition dkenv name ty term =
@@ -53,23 +45,27 @@ let compile_definition dkenv name ty term =
       Theorem (of_name name, CTerm.compile_term dkenv empty_env a, proof')
   | _ ->
       if is_tyop ty then
-        (* Format.eprintf "[COMPILE] typeop: %a@." Pp.print_name name ; *)
         let vars, ty = CType.compile_type_definition dkenv empty_env term in
         TypeDef (of_name name, vars, ty)
       else assert false
 
 let compile_entry env entry =
-  let module Pp = Api.Pp.Default in
   let md = Api.Env.get_name env in
   match entry with
   | Parsers.Entry.Decl (l, id, sc, st, ty) ->
       Api.Env.declare env l id sc st ty;
-      log_sttfa ~lvl:6 "compiling decl [%a]" Pp.print_ident id;
+      Logs.debug (fun m -> m "Compiling decl \"%a\"" Dpp.print_ident id);
       compile_declaration env (Basic.mk_name md id) ty
   | Def (l, id, sc, f, Some ty, te) ->
       Api.Env.define env l id sc f te (Some ty);
-      log_sttfa ~lvl:6 "compiling def [%a]" Pp.print_ident id;
+      Logs.debug (fun m -> m "Compiling def \"%a\"" Dpp.print_ident id);
       compile_definition env (Basic.mk_name md id) ty te
-  | Def _ -> exit_with "Definition without types are not supported"
-  | Rules _ -> exit_with "Rules are not part of the sttforall logic"
-  | _ -> exit_with "Dedukti Commands are not supported"
+  | Def _ ->
+      Logs.err (fun m -> m "Definition without types are not supported");
+      exit 1
+  | Rules _ ->
+      Logs.err (fun m -> m "Rules are not part of the sttforall logic");
+      exit 1
+  | Eval _ | Check _ | Infer _ | Print _ | DTree _ | Require _ | Name _ ->
+      Logs.err (fun m -> m "Dedukti Commands are not supported");
+      exit 1
